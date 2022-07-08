30c08a6260890af493bed820d0a1ea0bf1739ae9
Victor Villas <mail@victor.villas>
2022-07-08 19:18:05 -0300

    Move osm_vancouver to coffe search subquery

--- dbt/models/vancouver_coffee/osm_vancouver.sql
+++
@@ -1,9 +0,0 @@
-with vancouver as (
-  select geometry from `osm_walkability.gdal_administrative_regions`
-  where name = 'Vancouver'
-)
-select
-  planet_node.*
-from {{ source('osm', 'planet_nodes') }} as planet_node, vancouver
-where
-  ST_DWithin(vancouver.geometry, planet_node.geometry, 0)
--- dbt/models/vancouver_coffee/osm_vancouver_coffee_search.sql
+++ dbt/models/vancouver_coffee/osm_vancouver_coffee_search.sql
@@ -1,6 +1,18 @@
-select *
-from {{ ref('osm_vancouver') }}
-where exists (
+with vancouver as (
+  select geometry from {{ ref('gdal_administrative_regions') }}
+  where name = 'Vancouver'
+)
+select
+  planet_node.id,
+  (select value from unnest(all_tags) where key = 'name') as name,
+  planet_node.visible,
+  planet_node.osm_timestamp,
+  planet_node.geometry,
+  planet_node.all_tags
+from {{ source('osm', 'planet_nodes') }} as planet_node, vancouver
+where
+  st_dwithin(planet_node.geometry, vancouver.geometry, 0)
+  and exists (
   select * from unnest(all_tags)
   where key like '%coffee%' or value like '%coffee%'
     or key like '%cafe%' or value like '%cafe%'
