3752ec30c7d29ae75d758b8b43c9a89b11848b8d
Victor Villas <mail@victor.villas>
2022-07-13 16:20:58 -0300

    Remove apostrophe and round distance

--- dbt/macros/normalize.sql
+++ dbt/macros/normalize.sql
@@ -1,7 +1,7 @@
 {%- macro normalize(column_name) -%}
   trim(regexp_replace(regexp_replace(
     regexp_replace(lower({{ column_name }}), '[éè]', 'e'),
-    '(caffe|cafe|coffee)',
+    '(caffe|cafe|coffee|\')',
     ''
   ), r'\s+' , ' '))
 {%- endmacro -%}
--- dbt/models/pair_osm_vsi.sql
+++ dbt/models/pair_osm_vsi.sql
@@ -8,7 +8,7 @@ with pair_osm_vsi_ranked as (
     {{ normalize('vsi.business_name') }} as norm_vsi_name,
     osm.geometry as osm_geometry,
     vsi.geometry as vsi_geometry,
-    st_distance(vsi.geometry, osm.geometry) as distance,
+    round(st_distance(vsi.geometry, osm.geometry), 2) as distance,
     rank() over (partition by vsi.id order by st_distance(vsi.geometry, osm.geometry)) as rank
   from {{ ref('osm_coffee_search') }} as osm
   join {{ ref('vsi_coffee_search') }} as vsi
